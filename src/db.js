6eb4878e35d1f0dc6fc4ae2c9a0061ac510c822a
David White <webdigga42@gmail.com>
2017-07-24 20:36:15 +0100

    Initial add record code

--- src/db.js
+++ src/db.js
@@ -1,20 +1,34 @@
 var AWS = require("aws-sdk");
 
 AWS.config.update({
-  region: "eu-west-2",
-  endpoint: "https://dynamodb.eu-west-2.amazonaws.com",
-  credentials: {
-  	accessKeyId: 'AKIAIFDFWQ75XYJ4QB5Q',
-  	secretAccessKey: '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'
-  }
+	region: "eu-west-2",
+	endpoint: "https://dynamodb.eu-west-2.amazonaws.com",
+	credentials: {
+		accessKeyId: 'AKIAIFDFWQ75XYJ4QB5Q',
+		secretAccessKey: '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'
+	}
 });
 
 var db = new AWS.DynamoDB.DocumentClient();
 
-const params = {
+let params = {
 	TableName: 'shopping-list'
 };
 
-const getAllPostsPromise = db.scan(params).promise();
+const getAllRecordsPromise = db.scan(params).promise();
 
-exports.getAllPostsPromise = getAllPostsPromise;
+// params = {
+// 	TableName: 'shopping-list',
+// 	Item: {
+// 		'id': 1234,
+// 		'name': 'Boooommm!!!!'
+// 	}
+// }
+
+const addRecordPromise = db.put(params).promise();
+
+module.exports = {
+	params: params,
+	getAllRecordsPromise: getAllRecordsPromise,
+	addRecordPromise: addRecordPromise
+}
--- src/main.js
+++ src/main.js
@@ -9,20 +9,47 @@ class App extends Component {
 
 		this.state = {
 			items: []
-		}
+		};
 
 		this.getAllIngredients();
 	}
 
 	getAllIngredients() {
-		db.getAllPostsPromise.then(function( data ) {
+		db.getAllRecordsPromise.then(function( data ) {
 			this.setState({
 				items: data.Items
 			});
+		}).catch(function(err) {
+			console.log(err);
 		}.bind( this ));
+
+	}
+
+	addIngredient(id, name) {
+
+		
+		const params = db.params;
+
+		params.Item = {
+			'id': id,
+			'name': name
+		};
+
+		db.addRecordPromise.then(function(data) {
+			console.log('Success!!!!!');
+		}).catch(function(err) {
+			console.log(err);
+		});
+		
 	}
 
 	render() {
+
+		
+
+
+		this.addIngredient(4545, 'Booommmm!!!!');
+
 		return(
 			<div>
 				<List items={this.state.items} />
