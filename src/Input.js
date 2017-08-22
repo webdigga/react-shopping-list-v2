ed81c272b111a12fdcbc811d69451f485263c071
David White <webdigga42@gmail.com>
2017-08-22 18:33:31 +0100

    Adding ingredient on submit event

--- src/Input.js
+++ src/Input.js
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom';
 class Input extends Component {
 	constructor( props ) {
 		super( props );
+
+		this.state = {value: ''};
+
+		this.handleChange = this.handleChange.bind( this );
+		this.handleSubmit = this.handleSubmit.bind( this );
 	}
 
 	generateUUID() {
@@ -19,27 +24,36 @@ class Input extends Component {
 		return uuid;
 	}
 
-	onInputChange( ingredient ) {
+	handleChange( event ) {
+		this.setState({
+			value: event.target.value
+		});
+	}
+
+	handleSubmit( event ) {
+		event.preventDefault();
+
 		const Item = {
 			'id': this.generateUUID(),
-			'name': ingredient
+			'name': this.state.value
 		};
 
-		this.setState( {Item} );
+		this.setState({
+			Item
+		});
+
 		this.props.addIngredient( Item );
 	}
 
 	render() {
 		return (
-			<div>
-				<input
-					onChange={
-						event => {
-							this.onInputChange( event.target.value );
-						}
-					}
-				/>
-			</div>
+			<form onSubmit={this.handleSubmit}>
+				<label>
+					Name:
+					<input type="text" value={this.state.value} onChange={this.handleChange} />
+				</label>
+				<input type="submit" value="Submit" />
+			</form>
 		)
 	}
 }
